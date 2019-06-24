8ce3b4b0d6d70117ff3ba2b07fb6e3cd47d988d2
Thomas Mehoke <tmehoke@gmail.com>
2019-06-24 14:59:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# README
+
+This is the repository for mytax, a tool for building custom taxonomies, which can aid nucleotide sequence classification.
+
+# Installation
+
+Clone this repo with:
+
+`git clone https://github.com/tmehoke/mytax`
+
+Symbolically link all shell scripts into your path, for example with:
+
+`find mytax -name "*.sh" | while read fn; do sudo ln -s $PWD/$fn /usr/local/bin; done`
+
+# Dependencies
+
+ - jellyfish (version 1) - https://www.cbcb.umd.edu/software/jellyfish/
+ - kraken (version 1) - https://ccb.jhu.edu/software/kraken/
+ - gawk - https://www.gnu.org/software/gawk/manual/html_node/Installation.html
+ - perl
+ - GNU CoreUtils
+
+ - 16 GB of RAM is needed to build the provided influenza kraken database
+
+# Usage
+
+## Building example
+
+This pipeline is built from a central set of scripts.
+
+Build flu-kraken example with:
+
+`build_flukraken.sh -k flukraken-$(date +"%F")`
+
+The single script `build_flukraken.sh` functions as an outer wrapper for the influenza classification example using the Kraken classifier published in the mytax paper.
+
+
+`build_flukraken.sh` can also be used as a model to build modified pipelines as desired.  It is built from four main sub-modules:
+```
+	download_IVR.sh -> download references and taxonomy from IVR
+
+	build_IVR_metadata.sh -> build tab-delimited metadata table in format for mytax
+
+	build_taxonomy.sh -> build custom taxonomy from tab-delimited table
+
+	build_krakendb.sh -> add new taxonomic IDs to reference FASTA, build kraken database, post-process database for visualization pipeline
+```
+
+`build_krakendb.sh` currently references three helper scripts, which also need to be in the PATH:
+```
+	fix_references.sh -> adds new taxonomic IDs to reference FASTA
+
+	kraken-build -> builds kraken database
+
+	process_krakendb.sh -> post-processes database for visualization pipeline (not included in this repo yet)
+```
+
+# License and copyright
+
+Copyright (c) 2019 Thomas Mehoke
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
---
+++ build_flukraken.sh
@@ -0,0 +1,309 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------------------------------
+# script: build_flukraken.sh
+# author: Thomas Mehoke (thomas.mehoke@jhuapl.edu)
+# source: https://github.com/tmehoke/mytax
+
+# This script downloads and builds a custom influenza Kraken
+# classification database with a mytax taxonomy with the following levels:
+#  flu type
+#  segment
+#  subtype (A type only)
+#  host
+#  HA clade
+#  year
+#  strain
+
+#---------------------------------------------------------------------------------------------------
+# LICENSE AND DISCLAIMER
+#
+# Copyright (c) 2019 Thomas Mehoke
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#---------------------------------------------------------------------------------------------------
+
+# define colors for error messages
+red='\033[0;31m'
+RED='\033[1;31m'
+green='\033[0;32m'
+GREEN='\033[1;32m'
+yellow='\033[0;33m'
+YELLOW='\033[1;33m'
+blue='\033[0;34m'
+BLUE='\033[1;34m'
+purple='\033[0;35m'
+PURPLE='\033[1;35m'
+cyan='\033[0;36m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+# usage function
+usage() {
+	echo -e "usage: ${YELLOW}$0${NC} [options]"
+	echo -e ""
+	echo -e "OPTIONS:"
+	echo -e "   -h      show this message"
+	echo -e "   -k      directory to build kraken database"
+	echo -e "   -w      temporary directory (default: ${CYAN}/tmp${NC})"
+	echo -e ""
+if [[ 1 -eq 2 ]]; then
+	echo -e "  either specify"
+	echo -e ""
+	echo -e "   -d      download from IVR"
+	echo -e ""
+	echo -e "  or"
+	echo -e ""
+	echo -e "   -r      reference FASTA"
+	echo -e "   -t      taxonomy"
+	echo -e ""
+fi
+}
+
+gawk_install() {
+	echo -e "" >&2
+	echo -e "       ${RED}Please make sure gawk is installed.${NC}" >&2
+	echo -e "" >&2
+	echo -e "       Check: ${CYAN}https://www.gnu.org/software/gawk/manual/html_node/Installation.html${NC}" >&2
+	echo -e "              for more information" >&2
+	echo -e "" >&2
+}
+jellyfish_install() {
+	echo -e "" >&2
+	echo -e "       ${RED}Please make sure jellyfish version 1 is installed.${NC}" >&2
+	echo -e "" >&2
+	echo -e "       Check: ${CYAN}https://www.cbcb.umd.edu/software/jellyfish/${NC}" >&2
+	echo -e "              for more information" >&2
+	echo -e "" >&2
+	echo -e "       Download: ${CYAN}http://www.cbcb.umd.edu/software/jellyfish/jellyfish-1.1.11.tar.gz${NC}" >&2
+	echo -e "                 MD5: dc994ea8b0896156500ea8c648f24846" >&2
+	echo -e "" >&2
+}
+kraken_install() {
+	echo -e "" >&2
+	echo -e "       ${RED}Please make sure kraken version 1 is installed.${NC}" >&2
+	echo -e "" >&2
+	echo -e "       Check: ${CYAN}https://ccb.jhu.edu/software/kraken/${NC}" >&2
+	echo -e "              for more information" >&2
+	echo -e "" >&2
+	echo -e "       Clone git repository from: ${CYAN}https://github.com/DerrickWood/kraken.git${NC}" >&2
+	echo -e "" >&2
+}
+#---------------------------------------------------------------------------------------------------
+# set default values here
+offset1=1000000000
+offset2=2000000000
+download="true"
+logfile="/dev/null"
+tempdir="/tmp"
+prefix=""
+BASE=flukraken-$(date "+%F")
+
+#---------------------------------------------------------------------------------------------------
+# parse input arguments
+while getopts "hk:w:t:dl:x:" OPTION
+do
+	case $OPTION in
+		h) usage; exit 1 ;;
+		k) BASE=$OPTARG ;;
+		r) REFERENCES=$OPTARG ;;
+		t) TAXONOMY=$OPTARG ;;
+		l) logfile=$OPTARG ;;
+		w) tempdir=$OPTARG ;;
+		d) download="true" ;;
+		x) prefix=$OPTARG ;;
+		?) usage; exit ;;
+	esac
+done
+
+#---------------------------------------------------------------------------------------------------
+# check input arguments
+if [[ -z "$BASE" ]]; then
+	echo -e "${RED}Error: specify a kraken database with -k${NC}" >&2
+	usage
+	exit 2
+fi
+
+if [[ -d "$BASE" ]]; then
+	echo -e "${YELLOW}Warning: output directory \"$BASE\" already exists.${NC}" >&2
+fi
+
+if [[ "$download" == "false" ]]; then
+	if ! [[ -d "$TAXONOMY" ]]; then
+		echo -e "${RED}Error: must specify a taxonomy with -t if not downloading with -d${NC}" >&2
+		usage
+		exit 2
+	fi
+	if ! [[ -e "$REFERENCES" ]]; then
+		echo -e "${RED}Error: must specify a reference FASTA file with -r if not downloading with -d${NC}" >&2
+		usage
+		exit 2
+	fi
+fi
+
+#---------------------------------------------------------------------------------------------------
+# check required software is installed
+gawk_version=$(gawk --version 2> /dev/null | head -n1)
+jellyfish_version=$(jellyfish --version 2> /dev/null | head -n1)
+kraken_version=$(kraken --version 2> /dev/null | head -n1)
+
+if [[ -z "$gawk_version" ]]; then
+	echo -e "${RED}Error: gawk is not installed${NC}" >&2
+	gawk_install
+	usage
+	exit 2
+fi
+if [[ -z "$jellyfish_version" ]]; then
+	echo -e "${RED}Error: Jellyfish version 1 not installed${NC}" >&2
+	jellyfish_install
+	usage
+	exit 2
+elif [[ -z "$(echo "$jellyfish_version" | grep "jellyfish 1")" ]]; then
+	echo -e "${RED}Error: A version of jellyfish is installed, but not version 1.${NC}" >&2
+	echo -e "       $jellyfish_version" >&2
+	jellyfish_install
+	usage
+	exit 2
+fi
+if [[ -z "$kraken_version" ]]; then
+	echo -e "${RED}Error: Kraken is not installed${NC}" >&2
+	kraken_install
+	usage
+	exit 2
+fi
+
+#---------------------------------------------------------------------------------------------------
+# set up log file
+logfile="$BASE/build_flukraken.log"
+
+#===================================================================================================
+# DEFINE FUNCTIONS
+#===================================================================================================
+
+#---------------------------------------------------------------------------------------------------
+# log function that will output to STDOUT and a log file
+echo_log() {
+
+	input="$*"
+
+	# if input is non-empty string, prepend initial space
+	if [[ -n "$input" ]]; then
+		input=" $input"
+	fi
+
+	# print to STDOUT
+	echo -e "[$(date +"%F %T")]$prefix$input"
+
+	# print to log file (after removing color strings)
+	echo -e "[$(date +"%F %T")]$prefix$input" | gawk '{ printf("%s\n", gensub(/\x1b\[[0-9;]*m?/, "", "g", $0)); }' >> "$logfile"
+}
+
+#---------------------------------------------------------------------------------------------------
+
+#===================================================================================================
+# MAIN BODY
+#===================================================================================================
+
+# create output directory
+mkdir -m 775 -p "$BASE"
+
+echo_log "====== Call to ${YELLOW}"$(basename $0)"${NC} from ${GREEN}"$(hostname)"${NC} ======"
+
+# create directory to hold temporary files
+runtime=$(date +"%Y%m%d%H%M%S%N")
+workdir="$tempdir/$(basename $BASE)-$runtime"
+mkdir -m 775 -p "$workdir"
+
+echo_log "recording software version numbers"
+echo_log "  gawk version: $(gawk --version | head -n1)"
+echo_log "  jellyfish version: $(jellyfish --version | head -n1)"
+echo_log "  kraken version: $(kraken --version | head -n1)"
+
+if ! [[ -s $(which gawk) && -s $(which jellyfish) && -s $(which kraken) ]]; then
+	echo "Error: required packages are not installed" >&2
+	usage
+	exit 2
+fi
+
+echo_log "input arguments"
+echo_log "  working directory: ${CYAN}$workdir${NC}"
+echo_log "  threads: ${CYAN}1${NC}"
+echo_log "output arguments"
+echo_log "  kraken directory: ${CYAN}$BASE${NC}"
+echo_log "------ building flu-kraken database ------"
+
+#===================================================================================================
+# Download data from IVR
+#===================================================================================================
+
+if [[ "$download" == "true" ]]; then
+
+	download_IVR.sh \
+		-k "$BASE" \
+		-w "$workdir" \
+		-l "$logfile" \
+		-w "$workdir" \
+		-x " |  "
+
+	TAXONOMY="$BASE/taxonomy"
+	REFERENCES="$BASE/raw/influenza.fna"
+fi
+
+#===================================================================================================
+# Build metadata table for custom taxonomy
+#===================================================================================================
+
+if [[ "$download" == "true" ]]; then
+
+	build_IVR_metadata.sh \
+		-i "$BASE/raw/influenza_na.dat" \
+		-f "$BASE/raw/influenza.fna" \
+		-o "$BASE/raw/annotation_IVR.dat" \
+		-l "$logfile" \
+		-w "$workdir" \
+		-x " |  "
+fi
+
+#===================================================================================================
+# Build flu-specific taxonomy
+#===================================================================================================
+
+#-------------------------------------------------
+# Create flu-specific taxonomy
+build_taxonomy.sh \
+	-i "$BASE/raw/annotation_IVR.dat" \
+	-t "$TAXONOMY" \
+	-1 "$offset1" \
+	-2 "$offset2" \
+	-l "$logfile" \
+	-w "$workdir" \
+	-x " |  "
+
+#===================================================================================================
+# Create Kraken database
+#===================================================================================================
+
+#-------------------------------------------------
+# Fix FASTA headers to include new taxids
+build_krakendb.sh \
+	-k "$BASE" \
+	-r "$REFERENCES" \
+	-2 "$offset2" \
+	-l "$logfile" \
+	-w "$workdir" \
+	-x " |  "
+
+#-------------------------------------------------
+echo_log "${GREEN}Done${NC} (${YELLOW}"$(basename $0)"${NC})"
