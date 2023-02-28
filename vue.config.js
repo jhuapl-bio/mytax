13dc910fc2a8a7bfb8a8864c52115f2355d89719
Brian Merritt <brian.merritt@jhuapl.edu>
2023-02-28 03:22:08 -0500

    add public path for gh pages

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   transpileDependencies: [ 
     'vuetify'
   ],
+  publicPath: '/mytax',
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
     optimization: {
